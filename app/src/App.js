33350367ca0793a095800adc77c0c1d12eeca7b1
christopher <christopherbare@outlook.com>
2019-09-26 00:23:14 -0400

    added backend back to project

---
+++ app/src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import './App.css';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
+import { CookiesProvider } from "react-cookie";
+
+class App extends Component {
+    state = {
+        isLoading: true,
+        groups: []
+    };
+
+    async componentDidMount() {
+        const response = await fetch('/api/groups');
+        const body = await response.json();
+        this.setState({groups: body, isLoading: false});
+    }
+
+    render() {
+        return (
+        <CookiesProvider>
+            <Router>
+                <Switch>
+                    <Route path='/' exact={true} component={Home}/>
+                    <Route path='/groups' exact={true} component={GroupList}/>
+                    <Route path='/groups/:id' component={GroupEdit}/>
+                </Switch>
+            </Router>
+        </CookiesProvider>
+        )
+    }
+}
+
+export default App;
---
+++ src/main/java/com/example/reactspring/repository/GroupRepository.java
@@ -0,0 +1,10 @@
+package com.example.reactspring.repository;
+
+import com.example.reactspring.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Group findByName(String name);
+}
